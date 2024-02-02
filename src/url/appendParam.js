264a24407bc61d1b0b5f74fdb95448579853235b
唐钰惠 <tangyuhui@chongho.net>
2024-02-02 18:28:01 +0800

    feat: 更新拼接参数方法

--- src/url/appendParam.js
+++ src/url/appendParam.js
@@ -5,13 +5,21 @@
  * @LastEditTime: 2024-01-23 00:28:26
  */
 function appendParam(url, param,value) {
-    var separator;
-    if (url.indexOf('?') !== -1) {
-        separator = '&';
+    var parts = url.split('?');
+    if (parts.length > 1) {
+        var query = parts[1].split('&');
+        var updatedQuery = [];
+        for (var i = 0; i < query.length; i++) {
+            var pair = query[i].split('=');
+            if (pair[0] === param) {
+                pair[1] = value;
+            }
+            updatedQuery.push(pair.join('='));
+        }
+        return parts[0] + '?' + updatedQuery.join('&');
     } else {
-        separator = '?';
+        return url + '?' + param + '=' + value;
     }
-    return url + separator + param+ '=' + value;
 }
 
 module.exports = appendParam;
